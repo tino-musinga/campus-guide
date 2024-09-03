ed3f9215239b56033982508d438ca0538a32fab5
Tinotenda Musingarimi <tinomusinga@tino.local>
2024-09-03 16:14:30 -0400

    #GUIDE - Initialize Flask FrameWork

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,10 @@
-#flask backend here
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Campus Guide Chatbox"
+
+if __name__ == "__main__":
+    app.run()
